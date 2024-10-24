a561d1c27493d036f70f8ef1c445a11194796b0d
xon-g <xon0502@gmail.com>
2024-10-24 08:12:56 +0800

    remove unused tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,6 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <!-- Tailwind CSS -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
